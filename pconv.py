105b0ab5ea26070d6a485983bdda3ce9368d31e8
nathancorvussolis <nathancorvussolis@gmail.com>
2016-11-30 19:02:25 +0900

    Removed disused function

--- pconv.py
+++ pconv.py
@@ -5,16 +5,6 @@
 import codecs
 import sys
 
-def encodeq(s, enc):
-    e = b''
-    for c in s:
-        try:
-            e += c.encode(enc)
-        except UnicodeEncodeError:
-            e += b'?'
-    return e
-
-
 if __name__ == "__main__":
     if (len(sys.argv) != 5):
         print('usage: python pconv.py <enc_from> <inputfile> <enc_to> <outputfile>')
